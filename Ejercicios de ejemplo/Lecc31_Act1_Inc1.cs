ccdb7f1cc36ec227bdf8d4a91ffa9ecb4bd1e46e
20JCD01-JuanCabrera <p.cabrera.5bdb@gmail.com>
2026-07-02 19:25:46 -0600

    Add files via upload

---
+++ Ejercicios de ejemplo/Lecc31_Act1_Inc1.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int filas = 3;
+        int columnas = 5;
+        int i = 0;
+        int j = 0;
+
+        int[,] matriz = new int[3, 5];
+
+        for (i = 0; i < filas; i++)
+        {
+            for (j = 0; j < columnas; j++)
+            {
+                Console.WriteLine("Igresa el numero que quieres guardar en la fila: " + i + ", Columna: " + j + " =");
+                matriz[i, j] = int.Parse(Console.ReadLine());
+
+            }
+            for (i = 0; i < filas; i++)
+            {
+                for (j = 0; j < columnas; j++)
+                {
+                    Console.WriteLine("Fila: " + i + ", Columna: " + j + " = " + matriz[i, j]);
+                }
+
+            }
+
+
+        }
+    }
+}
